192398eb9c9d050fbf52cc3cb17b5c6dbc10ba62
Elavarasan2023IT014 <elavarasan.r2023it@sece.ac.in>
2025-01-22 14:45:44 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,69 @@
+
+const express = require('express')
+const app  = express()
+app.use(express.json())
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+const url = "mongodb://localhost:27017/Mern";
+port = 8001;
+mongoose
+    .connect(url)
+    .then(()=>{
+        console.log("DB connected")
+        app.listen(port,()=>{
+            console.log(`MY server is Running http://localhost:${port}`);
+        })
+    })
+const expenseSchema = new mongoose.Schema({
+    id:{type:String , required:true,unique:true},
+    title:{type:String,required:true},
+    amount:{type:Number,required:true},
+});
+
+const expenseModel = mongoose.model("expense-tracker",expenseSchema);//collection_name,schema_name
+
+app.post("/api/expense",async(req,res)=>{
+    const { title, amount} = req.body;
+    const newExpense = new expenseModel({
+        id:uuidv4(),
+        title:title,
+        amount:amount,
+    });
+    const savedExpense = await newExpense.save();
+    res.status(200).json(savedExpense);
+});
+
+app.put("/api/expense/:id",async(req,res)=>{
+    const {id}=req.params;
+    const {title,amount} =req.body;
+    const updateExpense = await expenseModel.findOneAndUpdate(
+        {
+            id:id,
+        },
+        {
+            title:title,
+            amount:amount,
+        },
+        {
+           new : true, 
+        }
+    )
+    res.status(200).json(updateExpense);
+})
+
+app.get("/api/expense",async (req,res)=>{
+    const exp=await expenseModel.find({});
+    res.json(exp);
+});
+
+app.get("/api/expense/:id",async (req,res)=>{
+    const {id}=req.params;
+    const exp=await expenseModel.find({id});
+    res.json(exp);
+});
+
+app.delete("/api/expense/:id",async (req,res)=>{
+    const {id} = req.params; 
+    const exp = await expenseModel.findOneAndDelete({id});
+    res.json(exp);
+});
